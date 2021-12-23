7e3098a26c0d1ea68c525398036fc74323574223
Daniel Schneider <daniel.schneider@eramux.com>
2021-12-23 12:09:28 +0100

    feat: add fitness first

--- src/main.js
+++ src/main.js
@@ -12,34 +12,39 @@ collectDefaultMetrics({ register });
 
 let gyms = [
   {
-    name: "www.boulderwelt-muenchen-ost.de",
+    name: "Boulderwelt Muenchen Ost",
     url: "https://www.boulderwelt-muenchen-ost.de/wp-admin/admin-ajax.php",
     parser: "boulderwelt"
   },
   {
-    name: "www.boulderwelt-muenchen-sued.de",
+    name: "Boulderwelt Muenchen Sued",
     url: "https://www.boulderwelt-muenchen-sued.de/wp-admin/admin-ajax.php",
     parser: "boulderwelt"
   },
   {
-    name: "www.boulderwelt-muenchen-west.de",
+    name: "Boulderwelt Muenchen West",
     url: "https://www.boulderwelt-muenchen-west.de/wp-admin/admin-ajax.php",
     parser: "boulderwelt"
   },
   {
-    name: "www.boulderwelt-dortmund.de",
+    name: "Boulderwelt Dortmund",
     url: "https://www.boulderwelt-dortmund.de/wp-admin/admin-ajax.php",
     parser: "boulderwelt"
   },
   {
-    name: "www.boulderwelt-frankfurt.de",
+    name: "Boulderwelt Frankfurt",
     url: "https://www.boulderwelt-frankfurt.de/wp-admin/admin-ajax.php",
     parser: "boulderwelt"
   },
   {
-    name: "www.boulderwelt-regensburg.de",
+    name: "Boulderwelt Regensburg",
     url: "https://www.boulderwelt-regensburg.de/wp-admin/admin-ajax.php",
     parser: "boulderwelt"
+  },
+  {
+    name: "Fitness First Haidhausen",
+    url: "https://www.fitnessfirst.de/club/api/checkins/muenchen6",
+    parser: "fitness_first"
   },
 ]
 
@@ -49,8 +54,9 @@ let gyms = [
 // let queueing_gauge = new client.Gauge({ name: 'gym_queueing', help: "Displays if a gym has a waiting queue at check-in", labelNames: ['domain'] });
 // let flevel_gauge = new client.Gauge({ name: 'gym_flevel', help: "Max gym occupation level according to corona guidelines", labelNames: ['domain'] });
 // let queue_gauge = new client.Gauge({ name: 'gym_queue', help: "Displays the amount of people that are currently waiting at check-in", labelNames: ['domain'] });
-let level_gauge = new client.Gauge({ name: 'gym_level', help: "The occupation level", labelNames: ['domain'] });
-let success_gauge = new client.Gauge({ name: 'gym_success', help: "Request success", labelNames: ['domain'] });
+let level_gauge = new client.Gauge({ name: 'place_percentage', help: "The occupation level in percent", labelNames: ['name'] });
+let allowed_gauge = new client.Gauge({ name: 'place_allowed', help: "The total allowed occupation for this place", labelNames: ['name'] });
+let current_gauge = new client.Gauge({ name: 'place_current', help: "The current occupation count of this place", labelNames: ['name'] });
 
 const timer = ms => new Promise(res => setTimeout(res, ms));
 
@@ -71,8 +77,7 @@ void async function () {
           }
         }).then((res) => {
           console.log("setting", gym.name);
-          level_gauge.labels({ domain: gym.name }).set(res.data.level)
-          success_gauge.labels({ domain: gym.name }).set(res.data.success ? 1 : 0)
+          level_gauge.labels({ name: gym.name }).set(res.data.level)
         }).catch(e => {
           console.log(e);
         })
@@ -84,8 +89,16 @@ void async function () {
           const $ = cheerio.load(res.data)
           const people = $('.actcounter [data-value]').html();
           const free = $('.freecounter [data-value]').html();
-          level_head_count.labels({ domain: gym.name }).set(people)
-          level_free_count.labels({ domain: gym.name }).set(free)
+          level_head_count.labels({ name: gym.name }).set(people)
+          level_free_count.labels({ name: gym.name }).set(free)
+        })
+      }
+
+      if (gym.parser == "fitness_first") {
+        axios.get(gym.url).then(res => {
+          console.log("setting", gym.name);
+          current_gauge.labels({ name: gym.name }).set(parseInt(res.data.data.check_ins))
+          allowed_gauge.labels({ name: gym.name }).set(parseInt(res.data.data.allowed_people))
         })
       }
     }
